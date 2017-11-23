b56fab05364cded65e03d302e66a1e0c21537ac7
ivillarreal <iva_arcangel@hotmail.com>
2017-11-23 17:10:33 -0600

    WIP

--- src/app/components/eventos/eventos.component.ts
+++ src/app/components/eventos/eventos.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray, FormControl } from '@angular/forms';
+
+import { Comision } from '../../models/comision';
+import { LocalStorageWorker } from '../../helpers/local-storage';
+import { Organizador } from '../../models/organizador';
+
 
 @Component({
   selector: 'app-eventos',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  eventForm: FormGroup;
+  _localStorage: LocalStorageWorker;
+  organizadores: Array<Organizador>;
+  comisiones: Array<any>;
 
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.organizadores = new Array<Organizador>();
+    this._localStorage = new LocalStorageWorker();
+    this.comisiones = new Array<any>();
+
+    this.eventForm = fb.group({
+      'nombre': ['', Validators.required],
+      'fecha': ['', Validators.required],
+      'lugar': ['', Validators.required],
+      'horario': ['', Validators.required]
+    });
   }
 
+  ngOnInit() {
+    this.organizadores = this.getOrganizadores();
+    this.getComisionesDefault();
+  }
+
+  saveEvent() { }
+
+  getOrganizadores(): any {
+    if (this._localStorage.get("organizadores")) {
+      return this._localStorage.get("organizadores")
+    } else {
+      return []
+    }
+  }
+
+  getComisionesDefault() {
+    let comisiones = this.getComisiones();
+    this.comisiones = new Array<any>();
+    if (comisiones.length > 0) {
+      for (let comision of comisiones) {
+        this.comisiones.push({ active: true, comision: comision });
+      }
+    }
+  }
+
+  getComisiones(): any {
+    if (this._localStorage.get("comisiones")) {
+      return this._localStorage.get("comisiones");
+    } else {
+      return [];
+    }
+  }
+
+  onChange(id) {
+    let organizador = this.organizadores.filter(organizador=>{
+      return organizador.id_organizador === parseInt(id);
+    });
+  }
 }
--- src/app/components/organizadores/organizadores.component.ts
+++ src/app/components/organizadores/organizadores.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray, FormControl } from '@angular/forms';
+
+import { Comision } from '../../models/comision';
+import { LocalStorageWorker } from '../../helpers/local-storage';
+import { Organizador } from '../../models/organizador';
+
 
 @Component({
   selector: 'app-organizadores',
@@ -7,9 +13,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizadoresComponent implements OnInit {
 
-  constructor() { }
+  organizadorForm: FormGroup;
+  _localStorage: LocalStorageWorker;
 
-  ngOnInit() {
+
+  comisionesOrg: Array<any>;
+
+  organizadores: Array<Organizador>;
+
+  name: string = "";
+
+  constructor(fb: FormBuilder) {
+    this._localStorage = new LocalStorageWorker();
+    this.organizadores = new Array<Organizador>();
+
+    this.organizadorForm = fb.group({
+      'name': ['', Validators.required]
+    });
   }
 
+  ngOnInit() {
+    this.getComisionesDefault();
+    this.organizadores = this.getOrganizadores();
+  }
+
+  saveOrganizador() {
+    if (this.organizadorForm.dirty && this.organizadorForm.valid) {
+      let comisiones = [];
+      for (let comisionOrg of this.comisionesOrg) {
+        if (comisionOrg.active) {
+          comisiones.push(comisionOrg.comision);
+        }
+      }
+      this.organizadores.push(new Organizador(this.organizadores.length + 1, this.name, comisiones));
+      this._localStorage.add("organizadores", this.organizadores);
+      this.organizadorForm.reset();
+      this.getComisionesDefault();
+    }
+  }
+  getComisionesDefault() {
+    let comisiones = this.getComisiones();
+    this.comisionesOrg = new Array<any>();
+    if (comisiones.length > 0) {
+      for (let comision of comisiones) {
+        this.comisionesOrg.push({ active: true, comision: comision });
+      }
+    }
+  }
+  getComisiones(): any {
+    if (this._localStorage.get("comisiones")) {
+      return this._localStorage.get("comisiones");
+    } else {
+      return [];
+    }
+  }
+
+  getOrganizadores(): any {
+    if (this._localStorage.get("organizadores")) {
+      return this._localStorage.get("organizadores")
+    } else {
+      return []
+    }
+  }
 }
